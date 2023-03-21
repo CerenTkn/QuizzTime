c6f6cf547bd54dfa09c9e1786144e98bab1b73d4
Ceren Tekin <cerentekin03@outlook.com>
2023-03-21 16:51:06 +0300

    quizcardactivity

--- app/src/main/java/com/example/quizztime/ui/QuizCardsActivity.kt
+++ app/src/main/java/com/example/quizztime/ui/QuizCardsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.quizztime.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.quizztime.R
 import com.example.quizztime.databinding.ActivityQuizCardsBinding
 
@@ -13,5 +16,15 @@ class QuizCardsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizCardsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupCardView()
+    }
+
+    private fun setupCardView(){
+        binding.cardflag.setOnClickListener {
+            Log.e("ceren", "flag card")
+            val intent = Intent(this@QuizCardsActivity, QuizTimeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
